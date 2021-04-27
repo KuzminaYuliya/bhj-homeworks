76e4296b724f3dd8d2fabcdfb096821a4741a1c0
KuzminaYuliya <kuzmina_h@mail.ru>
2021-04-27 20:28:15 +0500

    second commit for menu

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -6,22 +6,20 @@ let menu = Array.from(document.getElementsByClassName("menu_main"));
 
 menu.forEach(item => clickElement(item)); 
 
+
 function clickElement (item) {
 
     for (let index = 0; index < elementsLength; index++) {
-        let findMenu = elementsMenu[index].closest(".menu__item");
-        findMenu.onclick = function() {
+       elementsMenu[index].onclick = function() {
             let subMenu = Array.from(item.querySelectorAll(".menu_active"));
             subMenu.forEach(item => item.className = "menu menu_sub");
 
-            let subMenuElements = this.querySelector(".menu_sub");
+            let subMenuElements = this.closest(".menu__item").querySelector(".menu_sub");
                  
             if (subMenuElements !== null){
                 subMenuElements.className = "menu menu_sub menu_active";
                 return false;
             };
-                               
         };
     };
 };
-
