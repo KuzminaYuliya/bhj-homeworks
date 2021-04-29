13f0cfe2cfb44a48dc6ed234ee7b8e9ab6f60b39
KuzminaYuliya <kuzmina_h@mail.ru>
2021-04-29 20:16:22 +0500

    first commit for keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,13 +17,8 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода слова вызываем this.success()
-      При неправильном вводе символа - this.fail();
-     */
+    const checkEvent = (event) => (event.key.toLowerCase() === this.currentSymbol.textContent.toLowerCase()) ? this.success() : this.fail(); 
+    document.addEventListener('keyup', checkEvent);
   }
 
   success() {
