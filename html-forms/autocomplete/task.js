3f9c675fe6284087c5fd13ac03b47744c3d61e7c
KuzminaYuliya <kuzmina_h@mail.ru>
2021-05-07 20:17:26 +0500

    first commit for autocomplete

--- html-forms/autocomplete/task.js
+++ html-forms/autocomplete/task.js
@@ -68,25 +68,14 @@ class Autocomplete {
   }
 
   getMatches( text ) {
-    /*
-      TODO: этот метод нужно дописать
-      text - фраза, которую вводят в поле поиска
-      Метод должен вернуть массив.
-
-      Он формируется на основе списка опций select-элемента (this.input)
-      Подходящие опции - те, чей текст содержит то, что есть в аргументе text
-      Необходимо вернуть массив объектов со свойствами:
-      {
-        text: 'Содержимое <option>',
-        value: 'Содержимое атрибута value'
-      }
-    */
-    return [
-      {
-        text: 'Чубакка',
-        value: '1'
-      }
-    ];
+    let selectValues = new Array();
+    const selectList = Array.from(document.querySelector(".autocomplete__input").getElementsByTagName("option"));
+    for (var i = 0; i < selectList.length; i++) {
+        if (selectList[i].text.includes(text)) {
+            selectValues.push({text: selectList[i].text, value: selectList[i].value});
+        };
+    };
+    return selectValues;
   }
 }
 
