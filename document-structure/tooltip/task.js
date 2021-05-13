d083671d8082c73e73c537df1eab02c6cb9ecfde
KuzminaYuliya <kuzmina_h@mail.ru>
2021-05-13 13:39:36 +0500

    second commit for tooltip

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -2,24 +2,25 @@
 
 let elements = Array.from(document.querySelectorAll(".has-tooltip"));
 
-elements.forEach(item => {
-    let y = item.getBoundingClientRect().y + 25;
-    let x = item.getBoundingClientRect().x;
-    let divText = ` <div class="tooltip" style="left: ${x}px; top: ${y}px" >${item.title}</div>`;
-    item.insertAdjacentHTML("afterEnd",divText);
-}); 
+let divText = `<div class="tooltip" style="left: 0; top: 0" > Проверка! </div>`;
+document.body.insertAdjacentHTML("afterEnd",divText);
+let tooltip = document.querySelector(".tooltip")
+tooltip.setAttribute('data-position', 'bottom');
 
+let currentElement = {};
 const clickToolTip = function(event) {
     event.preventDefault();
-    let toolTips = document.querySelector(".tooltip_active");
-    if (toolTips !== null) {
-        toolTips.classList.remove("tooltip_active");
-    };
+    let y = event.currentTarget.getBoundingClientRect().y + 25;
+    let x = event.currentTarget.getBoundingClientRect().x;
+    tooltip.style.left = `${x}px`;
+    tooltip.style.top = `${y}px`;
+    tooltip.innerText = event.currentTarget.title;
+    if (currentElement === event.currentTarget) {
+        tooltip.classList.remove("tooltip_active");
+    }
+    else tooltip.classList.add("tooltip_active");
     
-    let curentToolTip = event.currentTarget.nextElementSibling;
-    curentToolTip.classList.add("tooltip_active");
-    curentToolTip.setAttribute('data-position', 'bottom');
-   
+    currentElement = event.currentTarget;
 };
 
 elements.forEach(item => item.addEventListener('click', clickToolTip)); 
